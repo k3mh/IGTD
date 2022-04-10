a1f63413cf37740dd011698c5cbc1389c6e92e05
kmoustafa <karim.moustafa@corvil.com>
2022-04-10 20:42:54 +0100

    get_IAT_score function

--- Evaluation.py
+++ Evaluation.py
@@ -131,3 +131,30 @@ def  overall_score (eval_all, ds_complexity, accu_list):
     total_scr = np.sum(score_lst)
     return  (total_scr, score_lst)
 
+
+def get_IAT_score(lib, results_df, dataset_complexity_vector):
+    for dataset_n in results_df.dataset.unique():
+        print(dataset_n)
+        #dataset_complexity_vector = list(range(1,12))
+        complexity_facotr = 100/sum(dataset_complexity_vector)
+
+        IAT_score= 0
+
+        con = "recall"
+        recall_ = results_df.loc[(results_df.lib == lib) & ( results_df.dataset == dataset_n) & (results_df["metric"] == con )].score.values[0]
+        con = "FPR"
+        FPR_ = results_df.loc[(results_df.lib == lib) & ( results_df.dataset == dataset_n) & (results_df["metric"] == con )].score.values[0]
+        con = "sensitivity"
+        sensitivity_ = results_df.loc[(results_df.lib == lib) & ( results_df.dataset == dataset_n) & (results_df["metric"] == con )].score.values[0]
+
+        # accuracy_ = model_performance.loc[model_performance.index + 1 == dataset_n, "accuracy"].values[0]
+        IAT_score_temp = (1 - FPR_ + recall_ + sensitivity_)  * dataset_complexity_vector[dataset_n-1] * complexity_facotr / 3
+        IAT_score = IAT_score + IAT_score_temp
+        print(recall_, FPR_, sensitivity_)
+        print(IAT_score_temp)
+    return IAT_score
+
+lime_IAT_score   = get_IAT_score(lib="lime", results_df=results_df, dataset_complexity_vector=list(range(1,12)))
+anchor_IAT_score =  get_IAT_score(lib="anchor", results_df=results_df, dataset_complexity_vector=list(range(1,12)))
+
+print(lime_IAT_score, anchor_IAT_score)
